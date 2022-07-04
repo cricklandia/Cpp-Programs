b6d1e0f0aa51cceebacaa997a7b8015d3bf2a30f
C Ram <cmstrickland@gmail.com>
2022-07-03 21:40:41 -0500

    Initial Commit

---
+++ Fun-With-Cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Fun-With-Cpp
+//
+//  Created by Christopher Strickland on 7/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
