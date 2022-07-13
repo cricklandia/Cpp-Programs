62d0728352a2806a18d67cbe7e3934733188b1a9
C Ram <cmstrickland@gmail.com>
2022-07-13 17:54:12 -0500

    add LL

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,8 @@
+//
+//  LinkedList.cpp
+//  Fun-With-Cpp
+//
+//  Created by Christopher Strickland on 7/13/22.
+//
+
+#include "LinkedList.hpp"
---
+++ LinkedList/LinkedList.hpp
@@ -0,0 +1,19 @@
+//
+//  LinkedList.hpp
+//  Fun-With-Cpp
+//
+//  Created by Christopher Strickland on 7/13/22.
+// a class for
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+#include <stdio.h>
+
+#endif /* LinkedList_hpp */
+
+
+class List{
+    
+    
+};
